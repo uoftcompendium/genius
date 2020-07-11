fa4bd836ccfefcbbe16878f83da0f331c36e6036
tank <35127914+tank0001@users.noreply.github.com>
2020-07-11 17:48:22 -0400

    Update genius.js

--- genius.js
+++ genius.js
@@ -57,11 +57,38 @@ client.on("message", async message => {
     message.channel.send("__**Other** storage unusually high is currently classified as this by Apple:__ \n\n> Non-removable mobile assets, like Siri voices, fonts, dictionaries, non-removable logs and caches, Spotlight index, and system data, such as Keychain and CloudKit Database. Cached files can't be deleted by the system.\n\n You can read more at <https://support.apple.com/en-us/HT201656> \n\n__The only known method to solve this issue is as follows:__ \n1) Backup your iPhone to **iCloud**. Note: you **cannot** use iTunes of Finer as high 'Other' storage will return. (<https://support.apple.com/en-us/HT203977>) \n2) Restore your phone to **factory settings** using iTunes or Finder on a computer. (<https://support.apple.com/en-us/HT201252>) \n3) Wait for the restore to finish, then you can safely restore from your **iCloud backup**. (<https://support.apple.com/en-us/HT204184>)");
   }
     
+  if(command === "ar") {
+    const sayMessage = args.join(" ");
+    message.channel.send("Account recovery is a process designed to get you back into your Apple ID account when you don’t have enough information to reset your password. For security reasons, it might take several days or longer before you can use your account again. <https://support.apple.com/en-us/HT204921>");
+  }
+    
+  if(command === "battery") {
+    const sayMessage = args.join(" ");
+    message.channel.send("<https://support.apple.com/en-us/HT208387>");
+  }
+      
+  if(command === "appdl") {
+    const sayMessage = args.join(" ");
+    message.channel.send("If you want to update apps, you can update them manually or turn on automatic updates (<https://support.apple.com/en-ca/HT202180>) But if your app won't update or is interrupted while it's downloading, try the steps at <https://support.apple.com/en-us/HT207165>.");
+  }
+      
+  if(command === "downgrade") {
+    const sayMessage = args.join(" ");
+    message.channel.send("iOS Developer & Public __Betas__ can be uninstalled by following the instructions at <https://support.apple.com/en-us/HT203282#beta>. An __iCloud backup__ is highly recommended.");
+  }
+      
+  if(command === "restore") {
+    const sayMessage = args.join(" ");
+    message.channel.send("A device can be **restored** through an __iCloud backup__ or __local (computer) backup__, by following instructions at <https://support.apple.com/en-us/HT204184>.");
+  }
+
+         
   if(command === "template") {
     const sayMessage = args.join(" ");
     message.channel.send("input");
   }
-    
+     
+  
 });
  
 client.login(process.env.ID);
