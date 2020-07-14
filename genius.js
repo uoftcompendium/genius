3a48bcaf897decc62a98d4761b5470f598258a04
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-07-14 14:41:22 -0400

    Update genius.js

--- genius.js
+++ genius.js
@@ -6,6 +6,94 @@ const client = new Discord.Client();
 
 // functions
 
+const reactionPages = async (message, author, options, page, retries) => {
+  // code incoming!
+  const filter = (reaction, user) => {
+    if (options.allowOtherUserReactions) {
+        return Object.values(options.emojis).includes(reaction.emoji.name);
+    } else {
+        return Object.values(options.emojis).includes(reaction.emoji.name) && user.id === author.id;
+    }
+  }
+
+  const collectorOptions = {
+      max: 1,
+      time: (options.timeLimit * 1000),
+      errors: ['time']
+  }
+
+
+  // THEN LOOP
+  message.awaitReactions(filter, collectorOptions)
+      .then(async (collected) => {
+          // more code here
+
+          const reaction = collected.first();
+          const minPage = 0;
+          const maxPage = (options.pages.length - 1);
+          const restartLoop = async () => { await reactionPages(message, author, options, page, retries); }
+
+          if (reaction.emoji.name === options.emojis.firstPage) {
+              // head back to the first page
+              if (page === minPage) return restartLoop();
+
+              page = minPage;
+              message = await message.edit(options.pages[minPage]);
+              return restartLoop();
+          }
+
+          if (reaction.emoji.name === options.emojis.previousPage) {
+              // move to the previous page
+              // move to the previous page
+              if (page === minPage) return restartLoop();
+
+              page--;
+              message = await message.edit(options.pages[page]);
+              return restartLoop();
+          }
+
+          if (reaction.emoji.name === options.emojis.stop) {
+              // stop listening
+              // stop listening
+              return true;
+          }
+
+          if (reaction.emoji.name === options.emojis.delete) {
+              // delete the message (also stops listening)   
+              // delete the message (also stops listening)
+              await message.delete();
+              return true;
+          }
+
+          if (reaction.emoji.name === options.emojis.nextPage) {
+              // move to the next page
+              if (page === maxPage) return restartLoop();
+
+              page++;
+              message = await message.edit(options.pages[page]);
+              return restartLoop();
+          }
+
+          if (reaction.emoji.name === options.emojis.lastPage) {
+              // head forward to the last page 
+              page = maxPage;
+              message = await message.edit(options.pages[maxPage]);
+              return restartLoop();      
+          }
+      })
+
+
+
+// CATCH LOOP
+      .catch(async (error) => {
+          // and some more here too
+      });
+
+
+
+
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -53,6 +141,19 @@ client.on('message', async (message) => {
       await msg.react(emojis.delete);
       await msg.react(emojis.nextPage);
       await msg.react(emojis.lastPage);
+
+      const options = {
+        emojis,
+        pages,
+        timeLimit,
+        maximumRetries,
+        allowOtherUserReactions
+      }
+      
+      // the difference:
+      // 'msg' is the message we sent from the bot
+      // 'message.author' is who sent the command initially
+      await reactionPages(msg, message.author, options, currentPage, currentRetries);
 
   }
 });
