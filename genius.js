2035bd98a8d81052e157356c5254e145eb63da94
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-07-13 22:57:57 -0400

    Update genius.js

--- genius.js
+++ genius.js
@@ -1,10 +1,23 @@
 const Discord = require('discord.js');
+const { token, prefix } = require('./config.json');
+
 const client = new Discord.Client();
-const config = require("./config.json");
 
+// functions
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
+client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
 
-
+    if (message.content === `${prefix}help`) {
+        // variables
+        
+        // command
+    }
+});
 
 client.login(process.env.ID);
