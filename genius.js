40ed62a4e51322f89b123828c3ded1337a5e800b
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-07-13 21:00:05 -0400

    Update genius.js

--- genius.js
+++ genius.js
@@ -13,6 +13,49 @@ client.on("message", async message => {
   if(message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+
+module.exports = {
+    name: 'page',
+    description: 'A command that clears a large amount of msgs',
+    execute(message, args) {
+        
+        let pages = [];
+        let page =1;
+
+        const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setFooter(`Page ${page} of ${pages.length}`)
+        .setDescription(pages [page-1])
+
+    message.channel.send(embed).then(msg => {
+    
+        msg.react('👈').then r => {
+            msg.react('👉')
+
+            const backwardsFilter = (reaction, user) => reaction.emoji.name === '👈' &&user.id === message.author.id;
+            const forwardsFilter =  (reaction, user) => reaction.emoji.name === '👉' &&user.id === message.author.id;
+
+            const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000});
+            const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000});
+
+            backwards.on('collect', r => {
+                if (page === 1) return;
+                page--;
+                embed.setDescription(pages [page-1]);
+                embed.setFooter(`Page ${page} of ${pages.length}`);
+                msg.edit(embed)
+            })
+
+            forwards.on('collect', r => {
+                if (page === 1) return;
+                page++;
+                embed.setDescription(pages [page-1]);
+                embed.setFooter(`Page ${page} of ${pages.length}`);
+            })
+        };
+    })
+    }
+}
   
   if(command === "ping") {
     const m = await message.channel.send("Recalculating...");
